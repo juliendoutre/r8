99c7799a9808f6ecb64d3f1a4d74c1820ef677db
Julien Doutre <jul.doutre@gmail.com>
2019-10-21 19:46:15 +0200

    Adding basic parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use r8;
+use std::env;
+use std::process;
 
 fn main() {
     r8::Cpu::new();
+
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("No rom path passed as argument, terminating program.");
+        process::exit(0);
+    }
+
+    println!("{}", args[1]);
 }
